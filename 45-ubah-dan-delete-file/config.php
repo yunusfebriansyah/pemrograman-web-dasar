738098af930f49a8054d79b41f1f4dbeeecc2ef4
yunusfebriansyah <febriansyahyunus369@gmail.com>
2023-02-02 01:42:06 +0700

    45 ubah dan delete file

---
+++ 45-ubah-dan-delete-file/config.php
@@ -0,0 +1,203 @@
+<?php
+  $koneksi = mysqli_connect('localhost', 'root', '', 'share_recipes');
+
+  function getAllRecipes()
+  {
+    global $koneksi;
+    $query = "SELECT * FROM vw_recipes WHERE title LIKE'%%'";
+    if( isset($_GET['search'])) :
+      $search = $_GET['search'];
+      $query = "SELECT * FROM vw_recipes WHERE title LIKE '%$search%'";
+    endif;
+    $query .= " ORDER BY id DESC";
+    if( isset($_GET['category'])) :
+      $category = $_GET['category'];
+      if( $category != "" ) :
+        $query .= " AND category_id = '$category'";
+      endif;
+    endif;
+    $result = mysqli_query($koneksi, $query);
+    $rows = [];
+    while( $row = mysqli_fetch_assoc($result) ){
+      $rows[] = $row;
+    }
+    return $rows;
+  }
+
+  function getDetailRecipe()
+  {
+    if( isset($_GET['id']) ) :
+      $id = $_GET['id'];
+      global $koneksi;
+      $query = "SELECT * FROM vw_recipes WHERE id=$id";
+      $result = mysqli_query($koneksi, $query);
+      $row = mysqli_fetch_object($result);
+      return $row;
+    else :
+      return null;
+    endif;
+  }
+
+  function getAllCategories()
+  {
+    global $koneksi;
+    $query = "SELECT * FROM categories";
+    $result = mysqli_query($koneksi, $query);
+    $rows = [];
+    while( $row = mysqli_fetch_assoc($result) ){
+      $rows[] = $row;
+    }
+    return $rows;
+  }
+
+  function upload( $image )
+  {
+    $fileName = $image['name'];
+    $tmpName = $image['tmp_name'];
+    $fileSize = $image['size'];
+    $error = $image['error'];
+    
+    // cek apakah filenya ada atau tidak
+    if( $error == 4 ):
+      echo "<script>
+            alert('Gambar harus ada!.');
+          </script>";
+      return false;
+    endif;
+
+    // cek apakah file yg di upload tipe nya gambar atau bukan
+    $imageFormatValid = ['jpg', 'jpeg', 'png', 'webp', 'bmp'];
+    $imageFormat = explode('.', $fileName); 
+    $imageFormat = strtolower(end($imageFormat)); // JPEG => jpeg
+    if( !in_array($imageFormat, $imageFormatValid) ) :
+      echo "<script>
+            alert('File harus gambar!.');
+          </script>";
+      return false;
+    endif;
+
+    // cek file size
+    if( $fileSize > 2000000 ) :
+      echo "<script>
+            alert('Ukuran gambar maksimal 2MB!.');
+          </script>";
+      return false;
+    endif;
+
+    // eksekusi nama file baru
+    $newFileName = date('YmdHis') . uniqid() . '.'. $imageFormat;
+    move_uploaded_file($tmpName, 'assets/thumbs/' . $newFileName);
+
+    return 'assets/thumbs/' . $newFileName;
+  }
+
+  function addRecipe( $data )
+  {
+    global $koneksi;
+
+    $title = htmlspecialchars($data['title']);
+    $category_id = $data['category_id'];
+    $materials = $data['materials'];
+    $procedures = $data['procedures'];
+    $user_id = 2;
+
+    // uploa image
+    $thumbnail = upload($_FILES['thumbnail']);
+
+    // cek apakah validasinya lolos atau tidak
+    if( !$thumbnail ) :
+      // validasinya gak lolos
+      return false;
+    endif;
+    
+    // jalankan query
+    mysqli_query($koneksi, "INSERT INTO recipes VALUES (NULL, '$title' ,'$user_id', '$category_id', '$materials', '$procedures', '$thumbnail', 1)");
+
+    if(mysqli_affected_rows($koneksi) > 0) :
+      // return true kalo berhasil ditambah
+      return true;
+      else :
+        // return false kalo gagal ditambah
+      return false;
+    endif;
+  }
+
+  function checkRecipe( $id )
+  {
+    global $koneksi;
+    $query = "SELECT * FROM vw_recipes WHERE id = $id";
+    $result = mysqli_query($koneksi, $query);
+    $rows = [];
+    while( $row = mysqli_fetch_assoc($result) ){
+      $rows[] = $row;
+    }
+    return count($rows);
+  }
+
+  function deleteRecipe($id)
+  {
+    global $koneksi;
+    $query = "DELETE FROM recipes";
+    $recipe = getDetailRecipe();
+    $oldThumbnail = $recipe->thumbnail;
+    // kalo ada foto lama maka hapus
+    if( file_exists($oldThumbnail) and $oldThumbnail != 'assets/thumbs/thumb.jpg' ) :
+      unlink($oldThumbnail);
+    endif;
+    $query .= " WHERE id = $id";
+
+    /*
+      == Hidden
+      UPDATE recipes SET is_active = 0 WHERE id = $id;
+
+      maka gak perlu untuk hapus thumbnailnya
+    */
+
+    // jalankan query delete
+    mysqli_query($koneksi, $query);
+    if(mysqli_affected_rows($koneksi) > 0) :
+      // return true kalo berhasil dihapus
+      return true;
+      else :
+        // return false kalo gagal dihapus
+      return false;
+    endif;
+  }
+  
+  function changeRecipe($data)
+  {
+    global $koneksi;
+    $id = $data['id'];
+    $title = htmlspecialchars($data['title']);
+    $category_id = $data['category_id'];
+    $materials = $data['materials'];
+    $procedures = $data['procedures'];
+    
+    // query basic
+    $query = "UPDATE recipes SET title = '$title', category_id = '$category_id', materials = '$materials', procedures = '$procedures'";
+    
+    // cek apakah fotonya diganti atau tidak
+    if( $_FILES['thumbnail']['error'] === 0 ) :
+      $recipe = getDetailRecipe();
+      $oldThumbnail = $recipe->thumbnail;
+      $newThumbnail = upload($_FILES['thumbnail']);
+      // kalo ada foto lama maka hapus
+      if( file_exists($oldThumbnail) and $oldThumbnail != 'assets/thumbs/thumb.jpg' ) :
+        unlink($oldThumbnail);
+      endif;
+      $query .= ", thumbnail = '$newThumbnail'";
+    endif;
+
+    $query .= " WHERE id = '$id'";
+    
+    // jalankan query
+    mysqli_query($koneksi, $query);
+
+    if(mysqli_affected_rows($koneksi) > 0) :
+      // return true kalo berhasil ditambah
+      return true;
+      else :
+        // return false kalo gagal ditambah
+      return false;
+    endif;
+  }
---
+++ 45-ubah-dan-delete-file/ubah-resep.php
@@ -0,0 +1,103 @@
+<?php
+  $id = isset($_GET['id']) ? $_GET['id'] : null;
+  if( $id == null ) :
+    header('HTTP/1.1 403 Forbidden');die();
+  endif;
+
+  require_once 'config.php';
+
+  if( checkRecipe($id) > 1 or checkRecipe($id) < 1 ) :
+    header('HTTP/1.1 403 Forbidden');die();
+  endif;
+
+  if( isset($_POST['submit']) ) :
+    if(changeRecipe($_POST)) :
+      // berhasil
+      echo "<script>
+            alert('Resep kamu berhasil diubah');
+            location.href = 'index.php';
+          </script>";
+    else :
+      // gagal
+      echo "<script>
+            alert('Resep kamu gagal diubah');
+            location.href = 'index.php';
+          </script>";
+    endif;
+  endif;
+
+  $categories = getAllCategories();
+  $recipe = getDetailRecipe();
+  include_once 'templates/header.php';
+  include_once 'templates/navbar.php';
+?>
+
+<div class="container">
+
+  <!-- content -->
+  <h1 class="my-3">Ubah Data Resep Kamu</h1>
+
+  <form action="" method="POST" enctype="multipart/form-data">
+    <input type="hidden" name="id" value="<?= $recipe->id ?>">
+    <div class="row">
+      <div class="col-12 col-md-8 col-lg-7">
+        <div class="mb-3">
+          <label for="title" class="form-label">Judul Resep</label>
+          <input type="text" class="form-control" id="title" placeholder="Isi judul resep" name="title" value="<?= $recipe->title ?>">
+        </div>
+        <div class="mb-3">
+          <label for="category_id" class="form-label">Pilih Kategori</label>
+          <select class="form-select" name="category_id" id="category_id">
+            <?php foreach( $categories as $category ) : ?>
+            <option value="<?= $category['id'] ?>" <?php if( $category['id'] == $recipe->category_id): echo 'selected'; else: echo ''; endif; ?>><?= $category['name'] ?></option>
+            <?php endforeach; ?>
+          </select>
+        </div>
+        <div class="mb-3">
+          <label for="materials" class="form-label">Bahan-bahan</label>
+          <textarea id="materials" name="materials"><?= $recipe->materials ?></textarea>
+        </div>
+        <div class="mb-3">
+          <label for="procedures" class="form-label">Cara Pembuatan</label>
+          <textarea id="procedures" name="procedures"><?= $recipe->procedures ?></textarea>
+        </div>
+        <div class="mb-3">
+          <label for="thumbnail" class="form-label mb-0">Foto Thumbnail</label>
+          <p class="mb-2 text-success"><small>kosongkan jika tidak ingin mengganti thumbnail</small></p>
+          <img src="<?= $recipe->thumbnail ?>" alt="<?= $recipe->title ?>" style="height: 150px;" class="d-block mb-3 rounded">
+          <input type="file" name="thumbnail" id="thumbnail" class="form-control">
+        </div>
+        <div class="mb-3">
+          <button type="submit" class="btn btn-success" name="submit">Ubah Resep</button>
+        </div>
+      </div>
+    </div>
+  </form>
+
+  <!-- end content -->
+      
+</div>
+
+<script src="https://cdn.ckeditor.com/ckeditor5/35.3.2/classic/ckeditor.js"></script>
+<script>
+  function createCKEditor(element)
+  {
+    ClassicEditor
+    .create( element, {
+        toolbar: [ 'heading', '|', 'bold', 'italic', 'bulletedList', 'numberedList' ],
+        heading: {
+            options: [
+                { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
+                { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
+                { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' }
+            ]
+        }
+    } )
+    .catch( error => {
+        console.error( error );
+    } );
+  }
+  createCKEditor(document.querySelector( '#materials' ));
+  createCKEditor(document.querySelector( '#procedures' ));
+</script>
+<?php include_once 'templates/footer.php'; ?>
